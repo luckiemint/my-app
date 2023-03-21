3be6794b9ca0b958b4db0be0e1f03305d72f64c1
Lucky Mint <106146899+luckiemint@users.noreply.github.com>
2023-03-21 23:04:33 +0530

    Add files via upload

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,54 @@
-import Header from "./Header"
+import Header from "./Header";
 
 const Blogs = () => {
     return (
         <>
             <Header />
             <div className='mainContainer'>
-                <h5>Blogs</h5>
+                <h1 style={{ color: 'red' }}>React Lifecycle of Components</h1>
+
+                <p>Lifecycle of a component can be defined as the <i>series of methods that are <b>invoked in different stages</b> of the component’s existence.</i></p>
+                <p>
+                    A React Component can go through four stages of its life as follows.
+                    <div className="imageContainer">
+                        <div>
+                            <ol>
+                                <b>
+                                    <li>Initialization</li>
+                                    <li>Mounting</li>
+                                    <li>Updating</li>
+                                    <li>Unmounting</li>
+                                </b>
+                            </ol>
+                        </div>
+                        <div>
+                            <img src="https://cdn-media-1.freecodecamp.org/images/1*_drMYY_IEgboMS4RhvC-lQ.png" height={300} />
+                        </div>
+                    </div>
+                </p>
+
+                <div className="box">
+                    <p>
+                        <h3>Initialization</h3>
+                        In this phase, the developer has to define the props and initial state of the component, this is generally done in the constructor of the component.
+                    </p><br />
+                    <p>
+                        <h3>Mounting</h3>
+                        Mounting means putting elements into the DOM:
+                        Mounting is the phase of the component lifecycle when the initialization of the component is completed.
+                        The component is mounted on the DOM and rendered for the first time on the webpage.
+                    </p> <br />
+                    <p>
+                        <h3>Updation</h3>
+                        The next phase in the lifecycle is when a component is updated. A component is updated whenever there is a change in the component's state or props
+                    </p> <br />
+                    <p>
+                        <h3>Unmounting</h3>
+                        The next phase in the lifecycle is when a component is removed from the DOM(or unmounting)
+                    </p><br /></div>
+
+
+
             </div>
 
         </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <>
+
             <div className='header'>
                 <Link to="/">Home</Link>
-                <Link to="/blogs">Blogs</Link>
+                <Link to="/blogs">About React</Link>
                 <Link to="/contacts">Contact</Link>
             </div>
+            <button onClick={() => navigate(-1)}>Go Back</button>
             <Outlet />
         </>
     )
